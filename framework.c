db4e066b1591f306c3dd375bf83647d325f5996d
maxwell burson <maxwellburson@gmail.com>
2017-03-19 00:53:23 -0700

    framework is progressing

--- framework.c
+++ framework.c
@@ -1,21 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   framework.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mburson <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/13 02:06:42 by mburson           #+#    #+#             */
-/*   Updated: 2017/02/13 22:55:44 by aphan            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <libft.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
 #include <framework.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
 
 static void		handle_signal(int status)
 {
@@ -39,65 +29,87 @@ static void		handle_signal(int status)
 	RESET;
 }
 
-static void		handle_exit(t_result *result, int status)
+static void		handle_exit(int *failed, int status)
 {
 	if (status == 0)
 	{
 		GREEN;
-		result->passed++;
 		ft_putstr("OK");
 	}
 	else if (status == -1)
 	{
 		RED;
 		ft_putstr("KO");
+		*failed = *failed + 1;
 	}
 	else
 	{
 		ft_putstr("unknown function result with code: ");
 		ft_putnbr(status);
+		*failed = *failed + 1;
 	}
 	RESET;
 }
 
-void			print_header(void)
+void			print_result(int total, int failed)
 {
-	ft_putstr("*********************************\n");
-	ft_putstr("**      42 - Unit Tests      ****\n");
-	ft_putstr("*********************************\n");
-}
-
-void			print_result(t_result *result)
-{
-	ft_putnbr((int)(result->passed));
+	ft_putnbr(total - failed);
 	ft_putstr("/");
-	ft_putnbr((int)(result->tried));
+	ft_putnbr(total);
 	ft_putstr(" tests passed\n");
 }
 
-void			run_test(t_result *result, char const *name, int (*test)(void))
+void				test(_Bool last, char const *name,
+									int (*test)(void), char *output)
 {
+	static int	total;
+	static int	failed;
 	int		status;
 	pid_t	f;
+	int		pipefd[2];
 
-	result->tried++;
+	char	buff[1024];
+	int		ret;
+
+	total++;
 	ft_putstr(" > ");
 	ft_putstr(name);
 	ft_putstr(": ");
+
+	pipe(pipefd);
 	f = fork();
 	if (f == 0)
-		exit((*test)());
-	else if (f == -1)
-		ft_putstr("FORK FAILED");
-	else
 	{
-		wait(&status);
-		if (WIFSIGNALED(status))
-			handle_signal(WTERMSIG(status));
-		else if (WIFEXITED(status))
-			handle_exit(result, (int)(char)WEXITSTATUS(status));
-		else
-			ft_putstr("unknown wait result");
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[1]);
+		close(pipefd[0]);
+		exit((*test)());
 	}
+	else if (f == -1)
+	{
+		ft_putstr("FORK FAILED");
+	}
+	close(pipefd[1]);
+	wait(&status);
+	if (WIFSIGNALED(status))
+		handle_signal(WTERMSIG(status));
+	else if (WIFEXITED(status))
+		handle_exit(&failed, (int)(char)WEXITSTATUS(status));
+	else
+		ft_putstr("unknown wait result");
 	ft_putchar('\n');
+	if (output == NULL)
+	{
+		ft_putstr("output found when null goes here\n");
+	}
+	if (last)
+	{
+		print_result(total, failed);
+		total = 0;
+		failed = 0;
+	}
+	ret = read(pipefd[0], &buff, 1023);
+	buff[ret] = '\0';
+	printf("result:\n%s\n", buff);
+	close(pipefd[0]);
 }
--- framework.h
+++ framework.h
@@ -19,18 +19,9 @@
 # define GREEN write(1, "\x1b[32m", 5)
 # define RED write(1, "\x1b[31m", 5)
 # define INVERSE write(1, "\x1b[7m", 4)
-# define BOLD write(1, "\x1b[1m", 4)
+# define BOLD write(1, "\x1b[1m", 4);
 
-typedef struct		s_result
-{
-	size_t		passed;
-	size_t		tried;
-}					t_result;
-
-void				print_header(void);
-
-void				run_test(t_result *result, char const *name,
-														int (*test)(void));
-void				print_result(t_result *result);
+void				test(_Bool last, char const *name,
+									int (*test)(void), char *output);
 
 #endif
